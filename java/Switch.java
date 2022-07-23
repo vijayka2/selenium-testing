f6d5022351d41f301641acb5bcc9a706ad5b95cd
vijayka2 <vijaykadam029@gmail.com>
2022-07-23 21:55:40 +0530

    added java three programs

---
+++ java/Switch.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class Switch {
+
+	public static void main(String[] args) 
+	{
+		char a='E';
+		switch(a) 
+		{
+		case 'C':System.out.println("this is C");
+		break;
+		case 'D':System.out.println("this is D");
+		break;
+		case 'E':System.out.println("this is E");
+		break;
+		default:System.out.println("This is not C,D,E");
+
+		
+		
+		}
+  }
+
+}
+
+
---
+++ java/sum2.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class sum2
+{
+
+	public static void main(String[] args) {
+		
+			
+			int a=10,b=20,sum;
+			
+			sum=a+b;
+			System.out.println("the sum of number is"+sum);
+			
+					
+					
+		}
+	}
+
+
